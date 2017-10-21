6aedcb0dc765803be400b211b2a7a8c71cc3867f
umino1 <umino1@wp.pl>
2017-10-21 16:39:31 +0200

    Menu scroll shadow

--- js/app.js
+++ js/app.js
@@ -22,19 +22,29 @@ document.addEventListener('DOMContentLoaded', function() {
     
     function showHide() {
     
-    var textArea = this.previousElementSibling;
+        var textArea = this.previousElementSibling;
        
-    if (textArea.style.display === 'none' || textArea.style.display === '') {
-          textArea.style.display = 'block';
-          this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+        if (textArea.style.display === 'none' || textArea.style.display === '') {
+            textArea.style.display = 'block';
+            this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
         } else {
           textArea.style.display = 'none';
           this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>';
         }
-      }
+    }
     
     for (var i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', showHide);
-      }   
-       
+    }   
+     
+      
+
+    // Zadanie 3
+
+    var menu = document.querySelector('.navbar');
+
+    window.addEventListener('scroll', function() {
+        menu.style.borderBottom = '1px solid #7f7f7f';
+    });
+    
 });
